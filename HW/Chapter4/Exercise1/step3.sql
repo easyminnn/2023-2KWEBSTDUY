9cfe55461a8f2e46c9d46d9fd6ded80efe168c83
easyminnn <2001joe@korea.ac.kr>
2023-11-08 04:01:17 +0900

    commit 11.8

--- HW/Chapter4/Exercise1/step3.sql
+++ HW/Chapter4/Exercise1/step3.sql
@@ -1,12 +1,13 @@
-CREATE TABLE `courses` (
+CREATE TABLE `students` (
+    `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(20) NOT NULL,
-    'year' INT NOT NULL,
-    `department` INT NOT NULL,
-    'personal_num' INT NOT NULL,
-    `major` VARCHAR(8) NOT NULL,
-    `phone_num` VARCHAR(16) NOT NULL,
-    'address' VARCHAR(50) NOT NULL,
-    'sum_credit' INT DEFAULT 0,
-    'avg_grade' DOUBLE DEFAULT 0.0,
-    `is_attend` TINYINT(1) DEFAULT 1
+    `admission_year` INT NOT NULL,
+    `major` VARCHAR(20) NOT NULL,
+    `individual_number` INT NOT NULL,
+    `phone_number` VARCHAR(11) NOT NULL,
+    `address` VARCHAR(100) NOT NULL,
+    `total_credit` INT NOT NULL DEFAULT 0,
+    `avg_credit` DOUBLE NOT NULL DEFAULT 0.0,
+    `is_attending` TINYINT(1) NOT NULL DEFAULT 1,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
